239770882928852b3a309c717c76f60c6bb648bc
Anna Sokolova <31693238+calabiyauspace@users.noreply.github.com>
2022-01-14 12:34:35 +0300

    Update main.py

--- lr3/main.py
+++ lr3/main.py
@@ -1,8 +1,8 @@
-import quad
+from scipy.integrate import quad
 import timeit
 def integral(x, a, b):
   return x**2
 a = 0
 b = 0
-time = timeit.timeit(quad(integral, 1, 4, args=(a,b)), setup , number=100)
-print(I)
+time = timeit.timeit('quad(integral, 1, 4, args=(a,b))', 'from __main__ import integral, quad, a, b', number=100)
+print(time)
